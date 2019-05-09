08a4e56562b67a9e121623871a64c79a481d38d1
M@x <ruitao.mu@gmail.com>
2019-05-09 16:41:26 +0800

    Add test and HelloWorld

---
+++ test/helloworld/HelloWorld.java
@@ -0,0 +1,29 @@
+class HelloWorld {
+	
+	private int b (int a, int l) {
+		while (l-- > 0) a = a*2;
+		return a;
+	}
+	
+	public static void main(String args[]) {
+		
+		HelloWorld h = new HelloWorld();
+		int i = 7;
+		System.out.println("HelloWorld from TEST begin");
+		while (i-- > 0) {
+			int c = h.b(i, 3);
+			System.out.println("Hello, IOT world!" + i + "* 2^3 = " + c);
+
+		}
+		System.out.println("Waiting for 5 seconds...");
+		try {
+		Thread.sleep(5000);
+		
+		System.out.println("Hello again");
+		System.out.println("Waiting for 5 seconds...");
+		Thread.sleep(5000);
+		System.out.println("Done!");
+		}catch(Exception e) {
+		}
+	}
+}
