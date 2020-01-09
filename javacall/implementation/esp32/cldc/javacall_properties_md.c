33eee8e419c418ddc24f9079e6c592f0d47ec53a
solaryear <shaomaocai@163.com>
2020-01-09 17:33:53 +0800

    get system info from property

--- javacall/implementation/esp32/cldc/javacall_properties_md.c
+++ javacall/implementation/esp32/cldc/javacall_properties_md.c
@@ -8,13 +8,14 @@ short property_file_name[JAVACALL_MAX_FILE_NAME_LENGTH+1] = {'/','a','p','p','d'
 
 #ifdef ENABLE_DYNAMIC_PROP
 
-#define JC_PROP_BUFLEN (20)
+#define JC_PROP_BUFLEN (64)
 
 #if USE_ESP_MINI || USE_JOSH_EVB
 extern int joshvm_esp32_wifi_get_state(int* state);
+extern int joshvm_esp32_get_sys_info(char* info, int size);
 #endif
 javacall_result javacall_get_dynamic_property(const char* key, char** result) {
-	static char buff[JC_PROP_BUFLEN] = {0};
+	static char buff[JC_PROP_BUFLEN + 1] = {0};
 
 	if (key == NULL) {
 		return JAVACALL_FAIL;
@@ -29,6 +30,13 @@ javacall_result javacall_get_dynamic_property(const char* key, char** result) {
 		}
 		#endif
 		snprintf(buff, JC_PROP_BUFLEN, "%d", state);
+	} else if (!strcmp(key, "system.info")) {
+		buff[0] = 0;
+		#if USE_ESP_MINI || USE_JOSH_EVB
+		if (joshvm_esp32_get_sys_info(buff, JC_PROP_BUFLEN) != 0) {
+			buff[0] = 0;
+		}
+		#endif
 	} else if (!strcmp(key, "microedition.commports")) {
 		snprintf(buff, JC_PROP_BUFLEN, "COM0,COM1,COM2");
 	} else {
